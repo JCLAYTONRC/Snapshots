c54c5a50aa22530f6a0fe7d210089aa4d20d5d14
Your Name <you@email.com>
2022-06-06 13:00:27 -0500

    Final

--- app/src/main/java/com/example/snapshots/HomeFragment.kt
+++ app/src/main/java/com/example/snapshots/HomeFragment.kt
@@ -1,20 +1,150 @@
 package com.example.snapshots
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.snapshots.databinding.FragmentHomeBinding
+import com.example.snapshots.databinding.ItemSnapshotBinding
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.firebase.ui.database.SnapshotParser
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 
-class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+class HomeFragment : Fragment(), HomeAux {
+
+    private lateinit var mBinding: FragmentHomeBinding
+
+    private lateinit var mFirebaseAdapter: FirebaseRecyclerAdapter<Snapshot, SnapshoHolder>
+    private lateinit var mLayoutManager: RecyclerView.LayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        mBinding = FragmentHomeBinding.inflate(inflater,container,false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val query = FirebaseDatabase.getInstance().reference.child("snapshots")
+
+        val option =
+        FirebaseRecyclerOptions.Builder<Snapshot>().setQuery(query, {
+            val snapshot = it.getValue(Snapshot::class.java)
+            snapshot!!.id = it.key!!
+            snapshot
+        }).build()
+
+
+        mFirebaseAdapter = object : FirebaseRecyclerAdapter<Snapshot, SnapshoHolder>(option){
+            private lateinit var mContext : Context
+
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SnapshoHolder {
+                mContext = parent.context
+
+                val view = LayoutInflater.from(mContext)
+                    .inflate(R.layout.item_snapshot,parent,false)
+                return  SnapshoHolder(view)
+            }
+
+            override fun onBindViewHolder(holder: SnapshoHolder, position: Int, model: Snapshot) {
+                val snapshot = getItem(position)
+                with(holder){
+                    setListener(snapshot)
+
+                    binding.tvTitle.text = snapshot.title
+                    binding.cbLike.text = snapshot.likeList.keys.size.toString()
+                    FirebaseAuth.getInstance().currentUser?.let {
+                        binding.cbLike.isChecked = snapshot.likeList
+                            .containsKey(it.uid)
+                    }
+
+                    Glide.with(mContext)
+                        .load(snapshot.photoUrl)
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .centerCrop()
+                        .into(binding.imgPhoto)
+                }
+
+            }
+
+            @SuppressLint("NotifyDataSetChanged") //Error interno firebase ui 8.0.0
+            override fun onDataChanged() {
+                super.onDataChanged()
+                mBinding.progressBar.visibility = View.GONE
+                notifyDataSetChanged()
+            }
+
+            override fun onError(error: DatabaseError) {
+                super.onError(error)
+                Toast.makeText(mContext,error.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        mLayoutManager = LinearLayoutManager(context)
+
+        mBinding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = mLayoutManager
+            adapter = mFirebaseAdapter
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mFirebaseAdapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mFirebaseAdapter.stopListening()
+    }
+
+    override fun gotoTop() {
+        mBinding.recyclerView.smoothScrollToPosition(0)
+    }
+
+    private fun deleteSnapshot(snapshot: Snapshot){
+        val databaseReference = FirebaseDatabase.getInstance().reference.child("snapshots")
+        databaseReference.child(snapshot.id).removeValue()
+    }
+
+    private fun setLike(snapshot: Snapshot, checked:Boolean){
+        val databaseReference = FirebaseDatabase.getInstance().reference.child("snapshots")
+        if(checked){
+            databaseReference.child(snapshot.id).child("likeList")
+                .child(FirebaseAuth.getInstance().currentUser!!.uid).setValue(checked)
+        }else{
+            databaseReference.child(snapshot.id).child("likeList")
+                .child(FirebaseAuth.getInstance().currentUser!!.uid).setValue(null)
+        }
+    }
+
+    inner class SnapshoHolder(view: View) : RecyclerView.ViewHolder(view){
+        val binding = ItemSnapshotBinding.bind(view)
+
+        fun setListener(snapshot: Snapshot){
+            binding.btnDelete.setOnClickListener{
+                deleteSnapshot(snapshot)
+            }
+
+            binding.cbLike.setOnCheckedChangeListener { compoundButton, checked ->
+                setLike(snapshot,checked)
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/snapshots/MainActivity.kt
+++ app/src/main/java/com/example/snapshots/MainActivity.kt
@@ -1,24 +1,66 @@
 package com.example.snapshots
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.snapshots.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.gms.auth.api.Auth
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 21
 
     private lateinit var mBinding: ActivityMainBinding
 
     private lateinit var mActivityFragment: Fragment
     private lateinit var mFragmentManager: FragmentManager
 
+    private lateinit var mAuthListener: FirebaseAuth.AuthStateListener
+    private var mFirebaseAuth: FirebaseAuth? = null
+
+    private val authResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+
+        if(it.resultCode == RESULT_OK){
+            Toast.makeText(this,"Bienvenido...",Toast.LENGTH_SHORT).show()
+        }else {
+            if(IdpResponse.fromResultIntent(it.data) == null){
+                finish()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+        setupAuth()
         setUpBottomNav()
+    }
+
+    private fun setupAuth(){
+        mFirebaseAuth = FirebaseAuth.getInstance()
+        mAuthListener = FirebaseAuth.AuthStateListener {
+            val user = it.currentUser
+            if(user == null){
+                authResult.launch(AuthUI.getInstance().createSignInIntentBuilder()
+                    .setIsSmartLockEnabled(false)
+                    .setAvailableProviders(
+                        Arrays.asList(AuthUI.IdpConfig.EmailBuilder().build(),
+                            AuthUI.IdpConfig.GoogleBuilder().build())
+                    )
+                    .build())
+            }
+        }
     }
 
     private fun setUpBottomNav(){
@@ -57,5 +99,22 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        mBinding.bottomNav.setOnItemReselectedListener {
+            when(it.itemId){
+                R.id.action_home -> (homeFragment as HomeAux).gotoTop()
+            }
+        }
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        mFirebaseAuth?.addAuthStateListener(mAuthListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mFirebaseAuth?.removeAuthStateListener(mAuthListener)
+    }
+
 }
